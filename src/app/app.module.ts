eb0cbc8506d39de090421a30042b671e27de7be1
Vishesh <visheshsingh16@gmail.com>
2018-05-29 15:51:09 -0500

    building for production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AngularFireModule } from "angularfire2";
 import { AngularFirestoreModule } from "angularfire2/firestore";
-import { environment } from "../environments/environment";
+import { environment } from "../environments/environment.prod";
 
 import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./components/items/items.component";
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAslk2W2hDAeYJJZanQeBmt9nr91LnLti8",
+    authDomain: "fsang-prod.firebaseapp.com",
+    databaseURL: "https://fsang-prod.firebaseio.com",
+    projectId: "fsang-prod",
+    storageBucket: "fsang-prod.appspot.com",
+    messagingSenderId: "736733545880"
+  }
 };
