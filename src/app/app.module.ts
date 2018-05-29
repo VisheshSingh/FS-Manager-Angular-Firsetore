e27715e8309404412bdede584ef4242cd80ec755
Vishesh <visheshsingh16@gmail.com>
2018-05-29 14:16:23 -0500

    add-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { AngularFireModule } from "angularfire2";
 import { AngularFirestoreModule } from "angularfire2/firestore";
 import { environment } from "../environments/environment";
@@ -8,12 +9,19 @@ import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./components/items/items.component";
 
 import { ItemService } from "./services/item.service";
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { AddItemComponent } from "./components/add-item/add-item.component";
 
 @NgModule({
-  declarations: [AppComponent, ItemsComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
+  ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase, "angularFS"),
     AngularFirestoreModule
   ],
