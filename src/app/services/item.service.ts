14efb645f1d62c4ca8c50436c9bca9e0ae5aacb9
Vishesh <visheshsingh16@gmail.com>
2018-05-29 14:02:53 -0500

    USing snapshotChanges for getting id

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -7,6 +7,7 @@ import {
 
 import { Item } from "../models/item";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 @Injectable({
   providedIn: "root"
 })
@@ -15,7 +16,19 @@ export class ItemService {
   items: Observable<Item[]>;
 
   constructor(public afs: AngularFirestore) {
-    this.items = this.afs.collection("items").valueChanges();
+    // this.items = this.afs.collection("items").valueChanges();
+    this.items = this.afs
+      .collection("items")
+      .snapshotChanges()
+      .pipe(
+        map(changes => {
+          return changes.map(a => {
+            const data = a.payload.doc.data() as Item;
+            data.id = a.payload.doc.id;
+            return data;
+          });
+        })
+      );
   }
 
   getItems() {
